ef9bc77f99935a64d38eb8e58a3a95242f582e38
vinay verma <vineyvarmavv4095818@gmail.com>
2026-08-13 19:17:52 +0530

    Ui upgraded

--- database.py
+++ database.py
@@ -7,18 +7,12 @@ cursor.execute("""
 CREATE TABLE IF NOT EXISTS favorites (
 
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-
     title TEXT,
-
     date TEXT,
-
     image_url TEXT,
-
     explanation TEXT,
-
     media_type TEXT
-
-)
+    )
 """)
 
 conn.commit()
